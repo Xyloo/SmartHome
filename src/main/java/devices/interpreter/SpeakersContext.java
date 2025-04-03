bc514e1432508e71e5892639e9eb88272748fc0a
bartlomiejgluszczak <gluszczak.bartlomiej@gmail.com>
2025-04-03 14:44:17 +0200

    T4: missing files iterpreter

---
+++ src/main/java/devices/interpreter/SpeakersContext.java
@@ -0,0 +1,19 @@
+package devices.interpreter;
+
+import devices.impl.SmartDevice;
+import devices.impl.speakers.SmartSpeaker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpeakersContext {
+    private final Map<String, SmartSpeaker> speakersMap = new HashMap<>();
+
+    public void addDevice(String name, SmartSpeaker device) {
+        speakersMap.put(name.toLowerCase(), device);
+    }
+
+    public SmartSpeaker getDevice(String name) {
+        return speakersMap.get(name.toLowerCase());
+    }
+}
---
+++ src/main/java/devices/interpreter/expressions/SetSpeakerVolumeExpression.java
@@ -0,0 +1,31 @@
+package devices.interpreter.expressions;
+
+import devices.impl.SmartDevice;
+import devices.impl.lighting.LightingDevice;
+import devices.impl.speakers.SmartSpeaker;
+import devices.interpreter.Context;
+import devices.interpreter.Expression;
+
+public class SetSpeakerVolumeExpression  implements Expression {
+    private final String location;
+    private final int volume;
+
+    public SetSpeakerVolumeExpression(String location, int volume) {
+        this.volume = volume;
+        this.location = location;
+    }
+
+    @Override
+    public void interpret( Context context) {
+        SmartDevice speaker = context.getDevice(location);
+        if (speaker != null && speaker instanceof SmartSpeaker) {
+            ((SmartSpeaker) speaker).setVolume(volume);
+            util.SmartLogger.getInstance().log ("[SetSpeakerVolumeExpression] Setting volume to " + volume);
+        }
+        else {
+            util.SmartLogger.getInstance().log ("[SetSpeakerVolumeExpression] Device not found or not a smart speaker.");
+        }
+    }
+}
+
+
